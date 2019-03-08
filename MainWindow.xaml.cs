79aae75366441f537375910d2c01ccf7555fe82f
JesseStanley <47399968+JesseStanley@users.noreply.github.com>
2019-03-08 15:05:39 -0500

    Added stuff

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _184784ForstringExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            String myWord = "Hello";
+            String output = "";
+
+            for(int i = 0; i < myWord.Length; i++)
+            {
+                output += myWord[i].ToString()
+                    + Environment.NewLine;
+            }
+            lblOutput.Content = output +
+                Environment.NewLine + myWord;
+        }
+    }
+}
